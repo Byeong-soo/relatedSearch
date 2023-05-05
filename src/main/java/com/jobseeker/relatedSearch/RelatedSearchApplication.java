9b4d0a9e361f175765110467d1133a5bd779e47c
ubs <ubs4939@naver.com>
2023-05-05 18:37:27 +0900

    project create

---
+++ src/main/java/com/jobseeker/relatedSearch/RelatedSearchApplication.java
@@ -0,0 +1,13 @@
+package com.jobseeker.relatedSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatedSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelatedSearchApplication.class, args);
+	}
+
+}
